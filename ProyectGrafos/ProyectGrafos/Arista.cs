3046972cc901524f9f5abd63e998ed3c3be59ad9
AlanMartinez2004 <133147564+Ala12Martinez@users.noreply.github.com>
2024-08-06 14:24:32 -0500

    Prueba

---
+++ ProyectGrafos/ProyectGrafos/Arista.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectGrafos
+{
+    public class Arista
+    {
+        public Nodo Origen { get; set; }
+        public Nodo Destino { get; set; }
+        public int Peso { get; set; }
+
+        public Arista(Nodo origen, Nodo destino, int peso)
+        {
+            Origen = origen;
+            Destino = destino;
+            Peso = peso;
+        }
+
+    }
+
+}
---
+++ ProyectGrafos/ProyectGrafos/Grafo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectGrafos
+{
+    public class Grafo
+    {
+        public List<Nodo> Nodos { get; set; }
+        public List<Arista> Aristas { get; set; }
+
+        public Grafo()
+        {
+            Nodos = new List<Nodo>();
+            Aristas = new List<Arista>();
+        }
+
+        public void AgregarNodo(Nodo nodo)
+        {
+            Nodos.Add(nodo);
+        }
+
+        public void AgregarArista(Arista arista)
+        {
+            Aristas.Add(arista);
+        }
+    }
+
+}
